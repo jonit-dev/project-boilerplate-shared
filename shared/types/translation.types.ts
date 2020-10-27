9186c159ec65174bc8783620eefb716439dcf6df
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-10-26 22:05:17 -0700

    node modules gitignore

---
+++ shared/types/translation.types.ts
@@ -0,0 +1,20 @@
+export enum Entities {
+  Global = "global", // this one exists only in /translations folder
+  Auth = "auth",
+  Tasks = "tasks",
+  Users = "users",
+  Logs = "logs",
+}
+
+export enum AuthTranslationKeys {
+  INVALID_CREDENTIALS = "INVALID_CREDENTIALS",
+  FORM_INPUT_EMAIL_PLACEHOLDER = "FORM_INPUT_EMAIL_PLACEHOLDER",
+  FORM_INPUT_PASSWORD_PLACEHOLDER = "FORM_INPUT_PASSWORD_PLACEHOLDER",
+  LOGIN = "LOGIN",
+  CREATE_YOUR_ACCOUNT = "CREATE_YOUR_ACCOUNT",
+  FORGOT_PASSWORD = "FORGOT_PASSWORD",
+}
+
+export enum GlobalTranslationKeys {
+  WAIT_MESSAGE = "WAIT_MESSAGE",
+}
