b1eaef79857a87dcfc3128566280d12e2b097c8d
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-10-27 13:20:29 -0700

    rolled back to json

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-// Languages
-export * as authLang from "./translations/auth.lang";
-export * as globalLang from "./translations/global.lang";
-export * as usersLang from "./translations/users.lang";
-export * as logsLang from "./translations/logs.lang";
-
 // Types ========================================
 
 export * from "./types/translation.types";
--- src/translations/logs.lang.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  LOG_DELETE_NOT_FOUND: {
-    eng: "Only admins can access this route!",
-    ptBr: "Apenas administradores podem acessar esta rota.",
-  },
-};
