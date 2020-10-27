d124141b88395dc78cc5415f32182e61f28357e5
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-10-27 11:35:20 -0700

    test ts lang

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Readme to be done
+
+References: https://medium.com/cameron-nokes/the-30-second-guide-to-publishing-a-typescript-package-to-npm-89d93ff7bccd
---
+++ src/translations/test.lang.ts
@@ -0,0 +1,6 @@
+export const testStrings = {
+  TEST: {
+    eng: "Only admins can access this route!",
+    ptBr: "Apenas administradores podem acessar esta rota.",
+  },
+};
