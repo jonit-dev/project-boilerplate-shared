728cf058dcb9e280eda6c9d305ae3614d787c869
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-10-31 17:13:37 -0700

    fix try

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { TextHelper } from './libs/TextHelper';
-
 // Types ========================================
 export * from "./types/translation.types";
 export * from "./types/httpStatus.types";
 
 // Helpers ========================================
 
-export { TextHelper } from "./libs/TextHelper";
+export * from "./libs/TextHelper";
