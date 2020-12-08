b95258d61a8c42a716ac423f9fce13834ef603dd
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-12-07 21:51:18 -0800

    google oauth types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 export * from "./types/httpStatus.types";
 export * from "./types/user.types";
 export * from "./types/translation.types";
+export * from "./types/env.types";
+export * from './types/googleOAuth.types';
 
 // Helpers ========================================
 
